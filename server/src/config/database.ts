e5d4041fb6b9dbeae54da0eb2fae9529bf7d9fb3
Aaron Paul Suarez <81550623+arnplsrz@users.noreply.github.com>
2025-12-31 23:38:34 +0800

    Update Prisma instantiation script

--- server/src/config/database.ts
+++ server/src/config/database.ts
@@ -1,15 +1,10 @@
-import { PrismaClient } from "../../generated/prisma_client/client";
+import "dotenv/config";
 import { PrismaPg } from "@prisma/adapter-pg";
-import { Pool } from "pg";
+import { PrismaClient } from "../../generated/prisma_client/client";
 
-const connectionString = process.env.DATABASE_URL;
+const connectionString = `${process.env.DATABASE_URL}`;
 
-const pool = new Pool({ connectionString });
-const adapter = new PrismaPg(pool);
-
-const prisma = new PrismaClient({
-  adapter,
-  log: process.env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
-});
+const adapter = new PrismaPg({ connectionString });
+const prisma = new PrismaClient({ adapter });
 
 export default prisma;
