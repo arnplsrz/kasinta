b5b073c9779a7da137dc7c23d40c33d2c75d6011
Aaron Paul Suarez <81550623+arnplsrz@users.noreply.github.com>
2026-06-14 15:24:37 +0800

    fix(prisma): update ssl connection support

--- server/src/config/database.ts
+++ server/src/config/database.ts
@@ -1,10 +1,16 @@
 import "dotenv/config";
+import { Pool } from "pg";
 import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "../../generated/prisma_client/client";
 
-const connectionString = `${process.env.DATABASE_URL}`;
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ...(process.env.DATABASE_SSL === "true" && {
+    ssl: { rejectUnauthorized: false },
+  }),
+});
 
-const adapter = new PrismaPg({ connectionString });
+const adapter = new PrismaPg(pool);
 const prisma = new PrismaClient({ adapter });
 
 export default prisma;
